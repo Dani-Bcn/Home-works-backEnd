6496e6ec982c074bc11ddcc3048b858b6d0be191
unknown <nneodani@gmail.com>
2022-09-07 14:52:53 +0200

    Delete task done

--- routes/task.js
+++ routes/task.js
@@ -2,35 +2,42 @@ const router = require("express").Router()
 const Task =require("../models/Task")
 
 router.post("/", async (req,res,next)=>{
-const {name, image, description} = req.body
-try{
+    const {name, image, description} = req.body
+        try{
+            const task = Task.create( {name, image, description})
+            res.status(201).json({ data:task});
+        }catch(error){
+            console.log(error)
+        }
 
-    const task = Task.create( {name, image, description})
-    res.status(201).json({ data:task});
-
-}catch(error){
-    console.log(error)
-}
-
-})
-router.get("/", async (req,res,next)=>{
-  
-    try{    
-        const task = await Task.find({})
-        res.status(201).json({ data:task});    
-    }catch(error){
-        console.log(error)
-    }
-    
     })
-    router.get("/:id", async (req,res,next)=>{
-        const { id } = req.params
+    router.get("/", async (req,res,next)=>{    
         try{    
-            const task = await Task.findById(id)
+            const task = await Task.find({})
             res.status(201).json({ data:task});    
         }catch(error){
             console.log(error)
         }        
-     })
+    })
+
+    router.get("/:id", async (req,res,next)=>{
+        const { id } = req.params
+            try{    
+                const task = await Task.findById(id)
+                res.status(201).json({ data:task});    
+            }catch(error){
+                console.log(error)
+            }        
+    })
+    router.delete("/:id", async (req,res,next)=>{
+        const { id } = req.params
+            try{    
+                const task = await Task.findByIdAndDelete(id)
+                res.status(201).json({ data:task});    
+            }catch(error){
+                console.log(error)
+            }        
+    })
+
 
 module.exports = router;
